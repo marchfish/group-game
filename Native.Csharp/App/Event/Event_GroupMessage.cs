1352c833d9725954c1ed0c17bc9c724a29221bef
marchfish <379975597@qq.com>
2019-07-01 15:22:49 +0800

    First submission

---
+++ Native.Csharp/App/Event/Event_GroupMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Native.Csharp.App.EventArgs;
+using Native.Csharp.App.Interface;
+
+namespace Native.Csharp.App.Event
+{
+    class Event_GroupMessage : IReceiveGroupMessage
+    {
+        public void ReceiveGroupMessage(object sender, CqGroupMessageEventArgs e)
+        {
+            Common.CqApi.SendGroupMessage(e.FromGroup, e.Message);
+        }
+    }
+}
