bc8a62ad950a821c9def80afe4627bfdda3e0d46
latent-q <416424939@qq.com>
2019-07-06 01:33:19 +0800

    添加用户信息

---
+++ Native.Csharp/App/Configs/GameConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Native.Csharp.App.Configs
+{
+    public class GameConfig
+    {
+       public static string[] userInfo = { "角色名", "血量", "蓝量", "攻击力", "魔法力", "暴击率", "闪避率", "防御力", "等级", "等级称号", "特殊称号" };
+       public static string[] userInfoDefault = { "", "100", "100", "10", "10", "0", "0", "5", "1", "新手", "无" };
+    }
+}
